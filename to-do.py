3216aaf83fe50edb91dbe0daa688f9e61f78ee3e
storreyes <sailektorreyes@gmail.com>
2025-03-18 16:34:59 -0400

    Nuevos cambios al codigo, para su mejor estructuracion

--- to-do.py
+++ to-do.py
@@ -3,12 +3,24 @@ import streamlit as st
 
 principal = st.title("Lista de tarea")
 
-
+anadir_tarea = []
 row, row2 = st.columns(2)
 row3, row4 = st.columns(2)
 
+with st.form("añadir_tarea"):
+    tarea = row.text_input("")
+    boton1 = st.form_submit_button
+    boton2 = row2.button("➕ ", type="tertiary")
 
-lst = row.text_input("", placeholder="Lista de tarea")
 
-if row2.button("➕", type="tertiary"):
-    agree = st.checkbox(lst)
+if boton1:
+    if tarea:
+        anadir_tarea.append(tarea)
+    elif boton2:
+        st.error("Debes introducir una tarea.")
+
+
+for i, tarea in enumerate(anadir_tarea):
+    st.write(f"{i+1}:{tarea}")
+
+    # agree = st.checkbox(f"{i+1}:{tarea}")
