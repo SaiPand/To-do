8c047ad533963bf48279ce1db6b4692558a1f82c
storreyes <sailektorreyes@gmail.com>
2025-03-19 16:42:45 -0400

    Nuevos cambios al codigo, se le agrego nuevos botones el de guardar, edictar y borrar

--- to-do.py
+++ to-do.py
@@ -3,24 +3,39 @@ import streamlit as st
 
 principal = st.title("Lista de tarea")
 
-anadir_tarea = []
-row, row2 = st.columns(2)
-row3, row4 = st.columns(2)
 
-with st.form("añadir_tarea"):
-    tarea = row.text_input("")
-    boton1 = st.form_submit_button
-    boton2 = row2.button("➕ ", type="tertiary")
+# row, row2 = st.columns(2)
+# row3, row4 = st.columns(2)
+
+if "anadir_tarea" not in st.session_state:
+    st.session_state.anadir_tarea = []
+
+if "tarea" not in st.session_state:
+    st.session_state.tarea = ""
+
+with st.form("formulario_agregar_texto"):
+    st.session_state.tarea = st.text_input("")
+    boton1 = st.form_submit_button("➕")
 
 
 if boton1:
-    if tarea:
-        anadir_tarea.append(tarea)
-    elif boton2:
+    if st.session_state.tarea:
+        st.session_state.anadir_tarea.append(st.session_state.tarea)
+        st.session_state.tarea = ""
+    else:
         st.error("Debes introducir una tarea.")
 
 
-for i, tarea in enumerate(anadir_tarea):
-    st.write(f"{i+1}:{tarea}")
+for i, tarea in enumerate(st.session_state.anadir_tarea):
 
-    # agree = st.checkbox(f"{i+1}:{tarea}")
+    col1, col2, col3 = st.columns([6, 6, 2])
+
+    with col1:
+        edictar = st.text_input(f"tarea {i+1}", value=tarea, key=f"edictar_{i}")
+    with col2:
+        if st.button("Guardar", key=f"guardar_{i}"):
+            st.session_state.anadir_tarea[i] = edictar
+    with col3:
+        if st.button("Borrar", key=f"Borrar_{i}"):
+            del st.session_state.anadir_tarea[i]
+            st.rerun()
