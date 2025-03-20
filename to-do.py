87346018bf06cd7cc351424416e8ce4135d128ea
storreyes <sailektorreyes@gmail.com>
2025-03-20 16:47:14 -0400

    Nueva integraccion de botones y stylos

--- to-do.py
+++ to-do.py
@@ -4,18 +4,16 @@ import streamlit as st
 principal = st.title("Lista de tarea")
 
 
-# row, row2 = st.columns(2)
-# row3, row4 = st.columns(2)
-
 if "anadir_tarea" not in st.session_state:
     st.session_state.anadir_tarea = []
+
 
 if "tarea" not in st.session_state:
     st.session_state.tarea = ""
 
 with st.form("formulario_agregar_texto"):
-    st.session_state.tarea = st.text_input("")
-    boton1 = st.form_submit_button("➕")
+    st.session_state.tarea = st.text_input("", placeholder="Escribe tu siguiente tarea")
+    boton1 = st.form_submit_button("Agregar", type="secondary")
 
 
 if boton1:
@@ -28,14 +26,62 @@ if boton1:
 
 for i, tarea in enumerate(st.session_state.anadir_tarea):
 
-    col1, col2, col3 = st.columns([6, 6, 2])
+    col1, col2, col3, col4 = st.columns([1, 3, 2, 2])
 
     with col1:
-        edictar = st.text_input(f"tarea {i+1}", value=tarea, key=f"edictar_{i}")
+        check = st.checkbox("", key=f"check_{i}_{tarea}")
     with col2:
-        if st.button("Guardar", key=f"guardar_{i}"):
-            st.session_state.anadir_tarea[i] = edictar
+        edictar = st.text_input(
+            f"tarea {i+1}", value=tarea, key=f"edictar_{i}", disabled=False
+        )
+        if check:
+            st.session_state.anadir_tarea[i] = st.session_state.anadir_tarea[i]
+
     with col3:
-        if st.button("Borrar", key=f"Borrar_{i}"):
+        if st.button("💾 Guardar", key=f"guardar_{i}"):
+            st.session_state.anadir_tarea[i] = edictar
+            st.success("Texto actualizado")
+
+    with col4:
+        if st.button("🗑️ Borrar", key=f"Borrar_{i}"):
             del st.session_state.anadir_tarea[i]
             st.rerun()
+
+bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background-image: url("https://e1.pxfuel.com/desktop-wallpaper/521/789/desktop-wallpaper-monte-tepuy-roraima-paisajes-de-venezuela-mt-roraima.jpg");
+background-size: cover;
+}
+[data-testid="stHeader"]{
+  background-color: rgba(0,0,0,0);
+}
+.caption-style{
+  color: black;
+  text-align: justify;
+  font-size: 20px;
+}
+.prediction-style{
+  color: black;
+  text-align: justify;
+  font-size: 20px;
+}
+
+.description-style{
+  color: black;
+  text-align: justify;
+  font-size: 20px;
+}
+
+.block-container {
+    background-color: #FAFAFA;
+    margin: 25px;
+    border: 2px solid grey;
+    border-radius: 25px;
+    padding-top: 25px;
+}
+
+
+</style>
+"""
+st.markdown(bg_img, unsafe_allow_html=True)
